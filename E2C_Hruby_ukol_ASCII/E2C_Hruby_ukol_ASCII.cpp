2d2208e27bfd576e9b86ace5cc9727eb9e4586a4
Vojtech Hruby <vojtech.hruby.020@pslib.cz>
2022-02-06 17:02:59 +0100

    Přidejte soubory projektu.

---
+++ E2C_Hruby_ukol_ASCII/E2C_Hruby_ukol_ASCII.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	FILE* soubor = fopen("ASCII.txt", "w");
+
+	if (soubor == NULL) {
+		printf("ERROR: soubor nelze otevrit\n");
+	}
+	return 1;
+
+	int ascii = 0x40;
+
+	fprintf(soubor, "znak\n");
+
+		for (int i = 1; i <= 26; i++) {
+			ascii += 1;
+			fwrite(&ascii, sizeof(int), 1, soubor);
+			fprintf(soubor, "%x\n", ascii);
+		}
+}
+	
